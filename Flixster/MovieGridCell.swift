fe8a045286620fb7b772de3e5c85d80b1afd7912
Gerald Hoskins <geraldjhoskinsiii@gmail.com>
2020-10-04 15:46:37 -0400

    Flixster v2

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Gerald Hoskins on 10/4/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
